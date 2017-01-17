680ab5905805d52088b5ae70b35c0d216d482e75
Sang Saephan <sangsaephan510@yahoo.com>
2017-01-17 02:16:44 -0800

    Create interface

---
+++ RainyShine/WeatherVC.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  RainyShine
+//
+//  Created by Sang Saephan on 1/17/17.
+//  Copyright © 2017 Sang Saephan. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var weatherImageView: UIImageView!
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
