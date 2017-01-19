db0a0576fd05960a666e5c22d57745e33adb4434
Sang Saephan <sangsaephan510@yahoo.com>
2017-01-19 10:50:16 -0800

    Data model and constants

---
+++ RainyShine/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  RainyShine
+//
+//  Created by Sang Saephan on 1/19/17.
+//  Copyright © 2017 Sang Saephan. All rights reserved.
+//
+
+import Foundation
+
+let baseUrl = "http://api.openweathermap.org/data/2.5/weather?"
+let latitude = "lat="
+let longitude = "&lon="
+let appId = "&appid="
+let apiKey = "cc1eb346585f997e112a8041477bf38e"
+
+let currentWeatherUrl = "\(baseUrl)\(latitude)37.8044\(longitude)-122.2711\(appId)\(apiKey)"
